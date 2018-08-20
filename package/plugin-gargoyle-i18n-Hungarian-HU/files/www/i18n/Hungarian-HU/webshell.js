ae78f555a9c94c96f63163133f651578d2c2f784
ViBE <ViBE-HU@users.noreply.github.com>
2018-08-20 20:42:29 +0200

    webshell.js translation

---
+++ package/plugin-gargoyle-i18n-Hungarian-HU/files/www/i18n/Hungarian-HU/webshell.js
@@ -0,0 +1,8 @@
+/*
+ * UTF-8 (with BOM) Hungarian-HU text strings for webshell.sh html elements
+ */
+
+webS.Webs="Parancssor";
+webS.Cmd="Parancs";
+webS.Exe="Futtatás";
+webS.CmdWarn="Ne futtass beavatkozást igénylő vagy végtelen folyamatot elindító parancsot!";

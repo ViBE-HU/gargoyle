792d0de03e39305c4d3df89a176f2b699f0cac04
ViBE <ViBE-HU@users.noreply.github.com>
2018-08-20 20:56:27 +0200

    webmon.js translation

---
+++ package/plugin-gargoyle-i18n-Hungarian-HU/files/www/i18n/Hungarian-HU/webmon.js
@@ -0,0 +1,22 @@
+/*
+ * UTF-8 (with BOM) Hungarian-HU text strings for webmon.sh html elements
+ */
+
+webmS.mWMon="Előzmények";
+webmS.PrSect="Előzmények naplózási beállításai";
+webmS.EMon="Előzmények engedélyezése";
+webmS.NumSt="Naplózott oldalak száma";
+webmS.NumSr="Naplózott keresések száma";
+webmS.MnAH="Összes ügyfél naplózása";
+webmS.MnOnly="Csak az alábbiak naplózása";
+webmS.MnExcl="Az alábbiakat ne naplózza";
+webmS.SpcIP="IP vagy tartomány megadása";
+webmS.RctSt="Böngészési előzmények";
+webmS.RctSr="Keresési előzmények";
+webmS.DlWD="Előzmények letöltése";
+webmS.cmsep="Az adatok vesszővel elválasztva";
+webmS.dForm="[Utolsó megtekintés],[Ügyfél IP-címe],[Oldal címe/Keresési kulcsszó]";
+webmS.VSit="Előzmények letöltése";
+webmS.SRqst="Keresések letöltése";
+webmS.dCol=['Ügyfél', 'Időpont', 'Oldal címe'];
+webmS.sCol=['Ügyfél', 'Időpont', 'Keresési kulcsszó'];

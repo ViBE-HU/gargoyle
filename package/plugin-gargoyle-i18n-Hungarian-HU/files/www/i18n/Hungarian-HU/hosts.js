e2064471ec208ff0fedca80ac287e6f77dc25392
ViBE <ViBE-HU@users.noreply.github.com>
2018-08-20 22:10:51 +0200

    hosts.js translation

---
+++ package/plugin-gargoyle-i18n-Hungarian-HU/files/www/i18n/Hungarian-HU/hosts.js
@@ -0,0 +1,21 @@
+/*
+ * UTF-8 (with BOM) Hungarian-HU text strings for hosts.sh html elements
+ */
+
+hostsStr.mHosts="Csatlakozó ügyfelek";
+hostsStr.RefreshR="Frissítés gyakorisága";
+hostsStr.RInfo="Ez határozza meg a megjelenő adatok frissítésének gyakoriságát";
+hostsStr.CurrLeases="Aktív DHCP bérletek";
+hostsStr.ConWifiHosts="Csatlakoztatott vezeték nélküli ügyfelek";
+hostsStr.ActiveHosts="Ügyfelek aktív kapcsolatai";
+
+//javascript
+hostsStr.HostIP="IP-cím";
+hostsStr.HostMAC="MAC-cím";
+hostsStr.LeaseExp="Bérleti jog vége";
+hostsStr.Bitrate="sávszél.";
+hostsStr.Signal="Jelerősség";
+hostsStr.ActiveConx="Aktív TCP kapcs.";
+hostsStr.RecentConx="Korábbi TCP kapcs.";
+hostsStr.UDPConx="UDP kapcs.";
+hostsStr.Band="Sáv";

1492ee8be168d495ad586b55594c6835d40a4d0b
ViBE <ViBE-HU@users.noreply.github.com>
2018-08-20 21:59:00 +0200

    i18n.js translation

---
+++ package/plugin-gargoyle-i18n-Hungarian-HU/files/www/i18n/Hungarian-HU/i18n.js
@@ -0,0 +1,14 @@
+/*
+ * UTF-8 (with BOM) Hungarian-HU text strings for i18n-firstoot.sh & languages.sh html elements
+ */
+
+intS.LMSect="Nyelvi beállítások";
+
+//i18n.js javascript
+intS.Lang="Nyelv";
+intS.Desc="Leírás";
+intS.Upld="Feltöltés";
+intS.UpMsg="Nyelvi fájl feltöltése";
+intS.Acv8="Alkalmazás";
+intS.Actv="Aktív";
+intS.Instd="Telepítve";

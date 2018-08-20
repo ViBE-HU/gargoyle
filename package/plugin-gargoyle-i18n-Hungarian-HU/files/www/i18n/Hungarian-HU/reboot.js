b760f40d0742614d77e2cb996528a6886435d80d
ViBE <ViBE-HU@users.noreply.github.com>
2018-08-20 21:44:54 +0200

    reboot.js translation

---
+++ package/plugin-gargoyle-i18n-Hungarian-HU/files/www/i18n/Hungarian-HU/reboot.js
@@ -0,0 +1,22 @@
+/*
+ * UTF-8 (with BOM) Hungarian-HU text strings for reboot.sh html elements
+ */
+
+rbS.RbSect="Útválasztó újraindítása";
+rbS.SchRb="Újraindítás ütemezése";
+rbS.NoSch="Ütemezés kikapcsolva";
+rbS.RbSch="Ütemezés bekapcsolva";
+rbS.WillR="Útválasztó újraindítása...";
+rbS.EDay="Minden nap";
+rbS.EWek="Minden héten";
+rbS.EMnh="Minden hónapban";
+rbS.RDay="Ezen a napon";
+rbS.RHr="Ekkor";
+
+//javascript
+rbS.SysR="Útválasztó újraindítása...";
+rbS.Digs=".";
+rbS.LD1s=".";
+rbS.LD2s=".";
+rbS.LD3s=".";
+rbS.DaysWArr=["vasárnap", "hétfőn", "kedden", "szerdán", "csütörtökön", "pénteken", "szombaton"];

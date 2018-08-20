809d1f606bfa5171a3fa72c436ba4ead92c9d2a3
ViBE <ViBE-HU@users.noreply.github.com>
2018-08-20 21:25:46 +0200

    update.js translation

---
+++ plugin-gargoyle-i18n-Hungarian-HU/files/www/i18n/Hungarian-HU/update.js
@@ -0,0 +1,23 @@
+/*
+ * UTF-8 (with BOM) Hungarian-HU text strings for update.sh html elements
+ */
+
+upS.UpFrm="Rendszerfrissítés";
+upS.bkup="mentése";
+upS.Warn="A rendszer frissítése során egyes beállítások elvészhetnek. Ezért <em>erősen</em> ajánlott a beállítások biztonsági <a href='backup.sh'>mentése</a> a frissítési folyamat megkezdése előtt.</p><p>Ugyanakkor lehetőség van a korábbi beállítások megtartására a <em>Kísérlet a beállítások megtartására</em> beállítás bejelölésével. Vedd figyelembe, hogy főverziók közti váltás esetén hibák merülhetnek fel, ha az újabb változat jelentősen eltér a jelenlegitől. Köztes változatok esetében ennek a veszélye alacsony. Készíts biztonsági mentést mielőtt frissítenél!";
+upS.CGV="Gargoyle telepített változata";
+upS.SelF="Frissítő fájl";
+upS.Prsv="Kísérlet a beállítások megtartására";
+
+//javascript
+upS.SelErr="HIBA: Nincs kiválasztva fálj a frissítéshez.";
+upS.Warn2="Ez a művelet töröl minden beállítást és akár (\"téglásíthatja\") az eszközt, ha nem megfelelő a fájl. Biztos vagy benne, hogy folytatod?";
+upS.Uping="Szoftver feltöltése...";
+upS.UWait="Rendszerfrissítés...";
+upS.Rbtg="Sikeres rendszerfrissítés, újraindítás...";
+upS.UErr="Hiba történt: Az útválasztó nem frissíthető.";
+upS.UErrBB="A frissítés rendszertöltőt is tartalmaz. Ezzel az útválasztó nem frissíthető.";
+upS.brcmT="A fájl kizárólag .bin vagy .trx képfájl lehet";
+upS.ar71xxT="A fájl elnevezése általában sysupgrade.bin vagy hasonló";
+upS.othrT="A fájl kizárólag egy kombinált .img képfájl lehet";
+upS.mvebu="A fájl kizárólag sysupgrade.tar fájl lehet";

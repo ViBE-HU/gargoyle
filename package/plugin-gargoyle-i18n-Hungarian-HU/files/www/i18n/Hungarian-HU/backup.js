1abae5574430bae7422f458bad870061a3029ce0
ViBE <ViBE-HU@users.noreply.github.com>
2018-08-20 22:23:59 +0200

    backup.js translation

---
+++ package/plugin-gargoyle-i18n-Hungarian-HU/files/www/i18n/Hungarian-HU/backup.js
@@ -0,0 +1,17 @@
+/*
+ * UTF-8 (with BOM) Hungarian-HU text strings for backup.sh html elements
+ */
+
+bkS.mBackup="Mentés / visszaállítás";
+bkS.CurrC="Beállítások mentése";
+bkS.RestC="Beállítások visszaállítása";
+bkS.DfltC="Alapértelmezett beállítások";
+bkS.SelOF="Korábbi mentés visszaállítása";
+
+//javascript
+bkS.SelCErr="HIBA: Visszaállításhoz ki kell jelölnöd egy korábbi beállításokat tartalmazó fájlt.";
+bkS.EraseWarn="Ez a művelet törölni fog minden meglévő beállítást és felülírja azokat az újakkal. Biztos vagy benne, hogy folytatod?";
+bkS.UpingC="Beállítások betöltése...";
+bkS.LdOrig="Alapértelmezett beállítások visszaállítása...";
+bkS.FailErr="Sikertelen visszaállítás. Ellenőrizd, hogy a feltölteni kívánt fájl valóban egy korábbi Gargoyle által létrehozott fájl!";
+bkS.PrepBack="Beállítások mentése...";

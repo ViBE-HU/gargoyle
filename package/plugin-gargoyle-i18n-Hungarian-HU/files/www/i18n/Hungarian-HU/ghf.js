a4d1c1cc1b2c839eb7d6b63aecdb2fa15b5d5da3
ViBE <ViBE-HU@users.noreply.github.com>
2018-08-20 22:14:16 +0200

    ghf.js translation

---
+++ package/plugin-gargoyle-i18n-Hungarian-HU/files/www/i18n/Hungarian-HU/ghf.js
@@ -0,0 +1,6 @@
+/* UTF-8 (with BOM) strings for gargoyle_header_footer */
+
+ghf.title="Gargoyle Router kezelőfelület";
+ghf.desc="Gargoyle Router kezelőfelület";
+ghf.devn="Eszköz neve";
+ghf.waits="Beállítások alkalmazása...";

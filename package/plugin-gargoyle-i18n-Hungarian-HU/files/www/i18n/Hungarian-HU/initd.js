fc615e3a76a70be8fc2d6c9d506721c1430f58e4
ViBE <ViBE-HU@users.noreply.github.com>
2018-08-20 21:53:11 +0200

    initd.js translation

---
+++ package/plugin-gargoyle-i18n-Hungarian-HU/files/www/i18n/Hungarian-HU/initd.js
@@ -0,0 +1,11 @@
+/*
+ * UTF-8 (with BOM) Hungarian-HU text strings for about.sh html elements
+ */
+
+initdS.Services="Szolgáltatások";
+initdS.NoServ="Nincs egyetlen szolgáltatás sem!";
+initdS.ActServ="Szolgáltatások frissítése...";
+initdS.ServColumn=['Szolgáltatás', 'Automatikusan indítás', '', '', ''];
+initdS.ServStart="Szolgáltatás elindítása...";
+initdS.ServStop="Szolgáltatás leállítása...";
+initdS.ServRestart="Szolgáltatás újraindítása...";

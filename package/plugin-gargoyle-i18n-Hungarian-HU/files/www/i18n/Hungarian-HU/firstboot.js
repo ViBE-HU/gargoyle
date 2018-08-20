27e6bdcc8dc71e206ce1746b532856680a53b823
ViBE <ViBE-HU@users.noreply.github.com>
2018-08-20 22:02:12 +0200

    firstboot.js translation

---
+++ package/plugin-gargoyle-i18n-Hungarian-HU/files/www/i18n/Hungarian-HU/firstboot.js
@@ -0,0 +1,15 @@
+/*
+ * UTF-8 (with BOM) Hungarian-HU text strings for firstboot.sh html elements
+ */
+
+fbS.ISSect="Első indítás";
+fbS.npass="Új felügyeleti jelszó megadása";
+fbS.NPass="Jelszó ismét";
+fbS.Stz="Időzóna kiválasztása";
+fbS.Sla="Nyelv kiválasztása";
+fbS.SSet="Beállítások mentése";
+fbS.ULngF="Nyelvi fájl feltöltése";
+
+//javascript
+fbS.nopsErr="HIBA: Jelszó beállítása kötelező";
+fbS.pseqErr="HIBA: A jelszó nem egyezik";
